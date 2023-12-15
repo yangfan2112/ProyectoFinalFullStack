1a81ff5679ed3d79ce02bc00a4584c39d3df57af
yangfan2112 <145387043+yangfan2112@users.noreply.github.com>
2023-12-15 19:56:36 +0100

    Creando proyecto backend

---
+++ src/main/java/edu/cesur/fullstackProyecto/ProyectoFinalFullstackApplication.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstackProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cesur/fullstackProyecto/ProyectoFinalFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstackProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
